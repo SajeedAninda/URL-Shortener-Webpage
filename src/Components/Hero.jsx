7830522eeeebd99a0c220b5e2b151c30c9d70abb
SajeedAninda <sajaninda@gmail.com>
2024-07-14 16:52:00 +0600

    Completed the hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import illustration from "../assets/illustration-working.svg"
 
 const Hero = () => {
     return (
-        <div className='heroDiv'>
-            <div className='h-[80vh] w-[80%] mx-auto'>
-                <div className="textDiv">
-                    <h1 className='text-[80px] text-[#35323E] font-bold leading-[90px]'>
+        <div className='heroDiv relative overflow-hidden'>
+            <div className='h-[80vh] w-[80%] mx-auto flex justify-between items-center'>
+                <div className="textDiv flex-1">
+                    <h1 className='text-[70px] text-[#35323E] font-bold leading-[80px]'>
                         More than just <br /> shorter links
                     </h1>
 
@@ -16,6 +17,10 @@ const Hero = () => {
                     <button className=' mt-8 bg-[#2ACFCF] px-8 py-3 rounded-[40px] text-white text-[22px] font-bold hover:bg-[#2acfcfb4] transition-colors delay-75 ease-in'>
                         Get Started
                     </button>
+                </div>
+
+                <div className='imgDiv flex-1 absolute -right-32 overflow-hidden'>
+                    <img className='overflow-hidden' src={illustration} alt="" />
                 </div>
             </div>
         </div>
