50477583835de80604ca63a9376913994504966a
SajeedAninda <sajaninda@gmail.com>
2024-07-27 19:07:57 +0600

    Complete responsiveness done

--- src/Components/BoostLinks.jsx
+++ src/Components/BoostLinks.jsx
@@ -4,7 +4,7 @@ import "./boostLinks.css"
 const BoostLinks = () => {
     return (
         <div className='w-full boostBg py-12 bg-[#3b3054] flex flex-col justify-center items-center'>
-            <h1 className='text-[40px] font-bold text-white'>
+            <h1 className='text-[40px] font-bold text-white text-center lg:text-left'>
                 Boost your links today
             </h1>
             <button className=' mt-8 bg-[#2ACFCF] px-8 py-3 rounded-[40px] text-white text-[18px] font-bold hover:bg-[#2acfcfb4] transition-colors delay-75 ease-in'>
